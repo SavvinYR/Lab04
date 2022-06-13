2d991439889c74119ed891fdffe0c657a422549d
SavvinYR <yaroslav.savvin@mail.ru>
2022-06-13 23:24:48 +0300

    Build: release

--- lab04.cpp
+++ lab04.cpp
@@ -10,8 +10,8 @@
 #include <curl/curl.h>
 using namespace std;
 
-vector<double> input_numbers(size_t count);
-vector<double> input_numbers(istream& in, size_t count) { //функция ввода чисел
+vector<double> input_numbers(istream& in, size_t count)   //функция ввода чисел
+{
     vector<double> result(count);
     for (size_t i = 0; i < count; i++)
         in >> result[i];
@@ -21,18 +21,18 @@ vector<double> input_numbers(istream& in, size_t count) { //функция ввода чисел
 Input read_input(istream& in,bool prompt)
 {
     Input data;
-     size_t number_count;
+    size_t number_count;
     if(prompt)
         cerr << "Enter number count: ";
-     in >> number_count;
-     if(prompt)
-     {
-         cerr << "Enter numbers: ";
-     }
+    in >> number_count;
+    if(prompt)
+    {
+        cerr << "Enter numbers: ";
+    }
     data.numbers = input_numbers(in, number_count);
     if(prompt)
     {
-       cerr<<"bin count:";
+        cerr<<"bin count:";
     }
     in>>data.bin_count;
     data.number_count = number_count;
@@ -40,7 +40,8 @@ Input read_input(istream& in,bool prompt)
     return data;
 }
 
-size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
+size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx)
+{
     size_t data_size = item_size * item_count;
     stringstream* buffer = reinterpret_cast<stringstream*>(ctx);
     buffer->write(reinterpret_cast<const char*>(items), data_size);
@@ -48,92 +49,87 @@ size_t write_data(void* items, size_t item_size, size_t item_count, void* ctx) {
 }
 
 Input
-download(const string& address) {
+download(const string& address)
+{
     stringstream buffer;
     CURL* curl = curl_easy_init();
-    if(curl){
+    if(curl)
+    {
         CURLcode res;
         curl_easy_setopt(curl, CURLOPT_URL, address.c_str());
         curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, write_data);
         curl_easy_setopt(curl, CURLOPT_WRITEDATA, &buffer);
         res = curl_easy_perform(curl);
-        if (res!=CURLE_OK){
+        if (res!=CURLE_OK)
+        {
             cerr<<curl_easy_strerror(res);
             exit(1);
         }
         curl_easy_cleanup(curl);
 
     }
-   // std::cout << __FUNCTION__ << "():" << __LINE__ << std::endl;
+    // std::cout << __FUNCTION__ << "():" << __LINE__ << std::endl;
     return read_input(buffer, false);
 
 }
+
+
 int
 main(int argc, char* argv[])
 {
 
-Input input;
-
-if (argc > 2)
-   {
-       size_t generator;
-       Input input2;
-    string option = "-generate";
-      for (int i=0; i<argc; i++)
-      {
-        string t = argv[i];
-         if (option==t)
-            generator =atoi (argv[i+1]);
-      }
-      vector <double> num;
-      //double[] nums = new double[generator];
-      //delete [] nums;
-      for(int j=0; j<generator; j++)
-      {
-        //num[j] = j;
-        double value;
-        value = rand() % 3 +3;
-      //  cout << value << "\n";
-        input2.numbers.push_back(value);
-      }
-      input2.bin_count = 3;
-      input2.number_count = generator;
-      cout << input2.number_count << "\n" ;
-      cout << input2.bin_count << "\n";
-      for(int i =0; i < generator; i++)
-      { cout << input2.numbers[i] << " ";
-
-      }
- return 0;
+    Input input;
 
 
-
-}
 
     if (argc > 1)
     {
 
-        input = download(argv[1]);
-
+        size_t generator = 0;
+        int URL_position = 0;
+        bool generate_found = false;
+        string option = "-generate";
+        char* URL = "http://";
+        for (int i=0; i<argc; i++)
+        {
+            string t = argv[i];
+            if (option==t)
+            {
+                generator =atoi (argv[i+1]);
+                generate_found = true;
+            }
+            if (strstr(argv[i],URL) !=NULL)
+                URL_position = i;
+        }
+        if (URL_position == 0)
+        {
+            cout << "Incorrect input. URL not founded.";
+            return 0;
+        }
+        input = download(argv[URL_position]);
+        if (generate_found)
+            if (generator>0)
+            {
+                vector <double> num;
+                double value;
+                input.number_count = generator;
+                for(int j=0; j<generator; j++)
+                {
+                    value = rand() % input.bin_count +3;
+                    input.numbers.push_back(value);
+                }
+            }
+            else
+            {
+                cout << "Incorrect input. Example: Lab04.exe -generate 10 http://... or Lab04.exe http://... -generate 10.";
+                return 0;
+            }
     }
-    else{
-
+    else
+    {
         input = read_input(cin, true);
     }
-
-    //for (int i = 0; i < input.numbers.size(); i++) {
-    //    std::cout << "Data " << i << " " << input.numbers[i] << std::endl;
-    //}
-    //std::cout << "bin_count " << input.bin_count << std::endl;
-    //std::cout << "number_count " << input.number_count << std::endl;
-    //input.number_count=10;
-
-    //std::cout << __FUNCTION__ << "():" << __LINE__ << std::endl;
-
-    //std::cout << __FUNCTION__ << "():" << __LINE__ << std::endl;
     const auto bins = make_histogram(input);
-    //std::cout << __FUNCTION__ << "():" << __LINE__ << std::endl;
     show_histogram_svg( bins);
-    //std::cout << __FUNCTION__ << "():" << __LINE__ << std::endl;
     return 0 ;
 }
